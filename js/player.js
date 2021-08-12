2482882cae9c8ec7e5087a626bf9578a25a1d1e8
samriddhiii <73587066+samriddhiii@users.noreply.github.com>
2021-08-12 16:37:27 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,47 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score=0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+        console.log("udpate")
+    }
+
+    // getscore(){
+    //     var dbref= database.ref("score")
+    //     dbref.on("value", (data)=>{
+    //       this.score= data.val()
+    //     })
+    //   }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
